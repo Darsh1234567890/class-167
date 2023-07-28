306f05b00b28b3fa687da11434699851ddd14864
Darsh1234567890 <x00114@reading-school.co.uk>
2023-07-28 15:03:24 +0100

    added oceans

---
+++ activity.js
@@ -0,0 +1,11 @@
+continents = ["asia", "europe", "africa", "antartica", "north america", "south america", "australia"]
+
+for(var i = 0; i < continents.length; i++){
+    console.log(continents[i])
+}
+
+oceans = ["pacific", "artic", "indian", "atlantic", "southern ocean"]
+
+for(var i = 0; i < oceans.length; i++){
+    console.log(oceans[i])
+}
